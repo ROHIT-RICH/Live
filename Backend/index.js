b9d2401fb76dcda9eaedf99de641de87f6af4f37
Rohit Vaishya <168424765+ROHIT-RICH@users.noreply.github.com>
2025-06-04 04:36:46 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ var app = express();
 var port = 4000;
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-// const path = require("path");
+const path = require("path");
 
 // Enable CORS and cookies first
 app.use(cors({
@@ -30,12 +30,12 @@ app.use('/api', VideoRoutes);
 app.use('/commentApi', CommentRoutes);
 
 // // Serve frontend static files after API routes
-// app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(path.join(__dirname, "dist")));
 
-// // Serve index.html for any other route (React router)
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "index.html"));
-// });
+// Serve index.html for any other route (React router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(port, () => {
   console.log("Server Running on Port", port);
